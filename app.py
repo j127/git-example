5e0574b6dd0fe467c6161232b04d39cfc5883b6c
J. Cohen <hostelmarketing@gmail.com>
2015-09-16 16:06:29 -0700

    Finish basic app.py

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ def homepage():
     title = 'Saluton, Mondo!'
     return render_template('index.html', title=title)
 
+@app.route('/page/<page_id>')
+def page(page_id):
+    page = Page.objects(pk=page_id)
+    return render_template('page.html', page=page)
+
+if __name__ == '__main__':
+    app.run(debug=True)
