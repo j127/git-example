1418b378f365eddaca462b73e90dcceba40a547f
J. Cohen <hostelmarketing@gmail.com>
2015-09-16 16:07:23 -0700

    Add User routes

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ def homepage():
     title = 'Saluton, Mondo!'
     return render_template('index.html', title=title)
 
+@app.route('/user/<user_id>')
+def pages(user_id):
+    user = User.objects(pk=page_id)
+    return render_template('user.html', user=user)
+
 @app.route('/page/<page_id>')
-def page(page_id):
+def pages(page_id):
     page = Page.objects(pk=page_id)
     return render_template('page.html', page=page)
 
